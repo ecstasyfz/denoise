3f18fe0ca0ac40b21ae60a5603c7313a4a4d2c6f
ecstasyfz <ecstasyfz@gmail.com>
2018-12-26 23:08:42 +0900

    好像没问题了

--- train.py
+++ train.py
@@ -48,6 +48,12 @@ def main():
     # create the networks
     netG = Generator(NUM_CHANNELS).to(device)
     netD = Discriminator(NUM_CHANNELS).to(device)
+    # initialize parameters TODO uncertain
+    for p in netG.parameters():
+        p.data.normal_(std=0.03)
+    for p in netD.parameters():
+        p.data.normal_(std=0.03)
+
     print('# generator parameters:', sum(param.numel() for param in netG.parameters()))
     print('# discriminator parameters:', sum(param.numel() for param in netD.parameters()))
 
@@ -67,107 +73,109 @@ def main():
 
     # main loop
     print('Starting Training Loop...')
-    for epoch in range(1, NUM_EPOCHS + 1):
-        # create empty epoch results
-        train_result = []
-        val_result = []
-        # train part
-        train_bar = tqdm(train_loader)
-        # set train mode
-        netG.train()
-        netD.train()
-        # train loop
-        for batch, (real_image, noisy_image) in enumerate(train_bar):
-            # move to GPU
-            real_image = real_image.to(device)
-            noisy_image = noisy_image.to(device)
-            # update D network
-            fake_image = netG(noisy_image)
-            netD.zero_grad()
-            fake_out = netD(fake_image).mean()
-            real_out = netD(real_image).mean()
+    try:
+        for epoch in range(1, NUM_EPOCHS + 1):
+            # create empty epoch results
+            train_result = []
+            val_result = []
+            # train part
+            train_bar = tqdm(train_loader)
+            # set train mode
+            netG.train()
+            netD.train()
+            # train loop
+            for batch, (real_image, noisy_image) in enumerate(train_bar):
+                # move to GPU
+                real_image = real_image.to(device)
+                noisy_image = noisy_image.to(device)
+                # update D network
+                fake_image = netG(noisy_image)
+                netD.zero_grad()
+                fake_out = netD(fake_image).mean()
+                real_out = netD(real_image).mean()
 
-            D_loss = discriminator_criterion(fake_out, real_out)
-            D_loss.backward(retain_graph=True)
+                D_loss = discriminator_criterion(fake_out, real_out)
+                D_loss.backward(retain_graph=True)
 
-            optimizerD.step()
+                optimizerD.step()
 
-            # updata G network
-            netG.zero_grad()
+                # updata G network
+                netG.zero_grad()
 
-            G_loss = generator_criterion(fake_out, fake_image, real_image)
-            G_loss.backward()
+                G_loss = generator_criterion(fake_out, fake_image, real_image)
+                G_loss.backward()
 
-            optimizerG.step()
+                optimizerG.step()
 
-            # convert scalar tensor
-            G_loss = G_loss.item()
-            D_loss = D_loss.item()
-            real_out = real_out.item()
-            fake_out = fake_out.item()
+                # convert scalar tensor
+                G_loss = G_loss.item()
+                D_loss = D_loss.item()
+                real_out = real_out.item()
+                fake_out = fake_out.item()
 
-            # append result
-            train_result.append([epoch, batch, G_loss, D_loss, real_out, fake_out])
-
-            # update progress bar
-            train_bar.set_description(
-                desc='[Train %d/%d] | G_loss: %.4f | D_loss: %.4f | D(fake): %.4f | D(real): %.4f' % (
-                    epoch, NUM_EPOCHS, G_loss, D_loss, fake_out, real_out))
-
-        # stop grad for validation
-        with torch.no_grad():
-            # validation part
-            val_bar = tqdm(val_loader)
-            # set validation mode
-            netG.eval()
-            netD.eval()
-            # validation loop
-            for filename, images in val_bar:
-                # move tensors to GPU
-                ground_images = images.to(device).squeeze(0)
-                # excess the network
-                denoised_images = netG(ground_images)
-                # chunk tensor
-                ground_images = ground_images.chunk(6)
-                denoised_images = denoised_images.chunk(6)
-                # measure
-                psnrs = [10*log10(1/((denoised_images[i]-ground_images[0])**2).mean()) for i in range(6)]
-                ssims = [pytorch_ssim.ssim(ground_images[0], denoised_images[i]).item() * 100 for i in range(6)]
-                # # move tensors back to CPU
-                # ground_images = [image.cpu() for image in ground_images]
-                # denoised_images = [image.cpu() for image in denoised_images]
-                # generate imaging to output/images
-                filename = filename[0]  # filename return from loader is a tuple
-                png_name = filename[:-len(filename.split('.')[-1])-1] + '.jpg'
-                save_validation_images(os.path.join(output_path, 'images', 'epoch_%d_%s' % (epoch, png_name)), ground_images, denoised_images, psnrs, ssims)
                 # append result
-                for i in range(6):
-                    val_result.append([epoch, filename, i * 10, psnrs[i], ssims[i]])
+                train_result.append([epoch, batch, G_loss, D_loss, real_out, fake_out])
 
                 # update progress bar
-                val_bar.set_description(
-                    desc='[Validation for sigma=20 %d/%d] | PSNR: %.4f | SSIM: %.4f' % (
-                        epoch, NUM_EPOCHS, psnrs[2], ssims[2]))
+                train_bar.set_description(
+                    desc='[Train %d/%d] | G_loss: %.4f | D_loss: %.4f | D(fake): %.4f | D(real): %.4f' % (
+                        epoch, NUM_EPOCHS, G_loss, D_loss, fake_out, real_out))
 
-        # save part
-        # save model parameters
-        torch.save(netG.state_dict(), os.path.join(output_path, 'params', 'netG_epoch_%d.pth' % epoch))
-        torch.save(netD.state_dict(), os.path.join(output_path, 'params', 'netD_epoch_%d.pth' % epoch))
-        # update dateframe
-        train_df = train_df.append(pd.DataFrame(train_result, columns=train_columns))
-        val_df = val_df.append(pd.DataFrame(val_result, columns=val_columns))
+            # stop grad for validation
+            with torch.no_grad():
+                # validation part
+                val_bar = tqdm(val_loader)
+                # set validation mode
+                netG.eval()
+                netD.eval()
+                # validation loop
+                for filename, images in val_bar:
+                    # move tensors to GPU
+                    ground_images = images.to(device).squeeze(0)
+                    # excess the network
+                    denoised_images = netG(ground_images)
+                    # chunk tensor
+                    ground_images = ground_images.chunk(6)
+                    denoised_images = denoised_images.chunk(6)
+                    # measure
+                    psnrs = [10*log10(1/((denoised_images[i]-ground_images[0])**2).mean()) for i in range(6)]
+                    ssims = [pytorch_ssim.ssim(ground_images[0], denoised_images[i]).item() * 100 for i in range(6)]
+                    # # move tensors back to CPU
+                    # ground_images = [image.cpu() for image in ground_images]
+                    # denoised_images = [image.cpu() for image in denoised_images]
+                    # generate imaging to output/images
+                    filename = filename[0]  # filename return from loader is a tuple
+                    png_name = filename[:-len(filename.split('.')[-1])-1] + '.jpg'
+                    save_validation_images(os.path.join(output_path, 'images', 'epoch_%d_%s' % (epoch, png_name)), ground_images, denoised_images, psnrs, ssims)
+                    # append result
+                    for i in range(6):
+                        val_result.append([epoch, filename, i * 10, psnrs[i], ssims[i]])
+
+                    # update progress bar
+                    val_bar.set_description(
+                        desc='[Validation for sigma=20 %d/%d] | PSNR: %.4f | SSIM: %.4f' % (
+                            epoch, NUM_EPOCHS, psnrs[2], ssims[2]))
+
+                # save part
+                # save model parameters
+                torch.save(netG.state_dict(), os.path.join(output_path, 'params', 'netG_epoch_%d.pth' % epoch))
+                torch.save(netD.state_dict(), os.path.join(output_path, 'params', 'netD_epoch_%d.pth' % epoch))
+                # update dateframe
+                train_df = train_df.append(pd.DataFrame(train_result, columns=train_columns))
+                val_df = val_df.append(pd.DataFrame(val_result, columns=val_columns))
 
     # end loop
-    # save csv
-    train_df.set_index('epoch', inplace=True)
-    val_df.set_index('epoch', inplace=True)
-    train_df.to_csv(os.path.join(output_path, 'train_result.csv'))
-    val_df.to_csv(os.path.join(output_path, 'validation_result.csv'))
+    finally:
+        # save csv
+        train_df.set_index('epoch', inplace=True)
+        val_df.set_index('epoch', inplace=True)
+        train_df.to_csv(os.path.join(output_path, 'train_result.csv'))
+        val_df.to_csv(os.path.join(output_path, 'validation_result.csv'))
 
-    # append finished task name
-    open('finished_tasks.txt', 'a').write(now + '\n')
+        # append finished task name
+        open('finished_tasks.txt', 'a').write(now + '\n')
 
-    print('Training Finished!')
+        print('Training Finished!')
 
 
 if __name__ == '__main__':
